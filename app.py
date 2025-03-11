36f99b7bed9cb0b361974a35dce5209ec3a99631
Sebastian Tay <taysebastian95@gmail.com>
2025-03-11 23:41:55 +0800

    Add video upload functionality

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
-st.title("Title")
+
+st.title("Triage System")
+
+uploaded_file = st.file_uploader("Upload a video", type = ["mp4", "mov"])
+if uploaded_file:
+    with open("temp_video.mp4", "wb") as f:
+        f.write(uploaded_file.getbuffer())
+    st.video("temp_video.mp4")
+    
