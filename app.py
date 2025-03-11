7dc24dfee581aa69af360b576bb55878f670aa75
Sebastian Tay <taysebastian95@gmail.com>
2025-03-12 02:30:01 +0800

    Add requests post call to backend

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import requests
 
+URL = "localhost:8000/upload"
 
 st.title("Triage System")
 
@@ -7,5 +9,10 @@ uploaded_file = st.file_uploader("Upload a video", type = ["mp4", "mov"])
 if uploaded_file:
     with open("temp_video.mp4", "wb") as f:
         f.write(uploaded_file.getbuffer())
+
     st.video("temp_video.mp4")
+    with open("temp_video.mp4", "rb") as f:
+        response = requests.post(URL, {"file": {f}})
+
+    st.write(response)
     
