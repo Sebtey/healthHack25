e427cb428d81e8c75d7e48f482de180ae5bb4cf4
Sebastian Tay <taysebastian95@gmail.com>
2025-03-13 00:04:44 +0800

    Enable frontend connection with fastapi backend

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 import streamlit as st
 import requests
 
-URL = "localhost:8000/upload"
+URL = "http://localhost:8000/upload"
 
 st.title("Triage System")
 
 uploaded_file = st.file_uploader("Upload a video", type = ["mp4", "mov"])
 if uploaded_file:
-    with open("temp_video.mp4", "wb") as f:
-        f.write(uploaded_file.getbuffer())
-
     st.video("temp_video.mp4")
-    with open("temp_video.mp4", "rb") as f:
-        response = requests.post(URL, {"file": {f}})
+    responseJson = requests.post(URL, files = {"file": uploaded_file.read()})
 
-    st.write(response)
+    response = responseJson.json()
+
+    st.write(response) #TODO retrieve the output value from the corresponding output key of response
     
