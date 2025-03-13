c35a21b8a16e56af5a7109e739f921cb1af4a953
CJ-Lee01 <chunjie01@live.com>
2025-03-13 13:52:00 +0800

    Extract probabilities

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 import streamlit as st
 import requests
+import json
 
 URL = "http://localhost:8000/upload"
 
 st.title("Triage System")
+
+mapper = {
+    0: "Normal General Movements",
+    1: "Poor repertoire movements",
+    2: "Cramped synchronized movements",
+    3: "chaotic movements",
+    4: "Others"
+}
 
 uploaded_file = st.file_uploader("Upload a video", type = ["mp4", "mov"])
 if uploaded_file:
@@ -11,6 +20,8 @@ if uploaded_file:
     responseJson = requests.post(URL, files = {"file": uploaded_file.read()})
 
     response = responseJson.json()
+    result = []
+    for index, i in enumerate(response["model_Result"]):
+        result.append(f"{mapper[index]}: {i:03f}")
 
-    st.write(response) #TODO retrieve the output value from the corresponding output key of response
-    
+    st.write(result)  #TODO retrieve the output value from the corresponding output key of response
