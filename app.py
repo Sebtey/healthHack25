ee5966d74225d9aed21cd4210a929dada6730bdf
CJ-Lee01 <chunjie01@live.com>
2025-03-13 13:38:10 +0800

    Comment out buggy code

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.title("Triage System")
 
 uploaded_file = st.file_uploader("Upload a video", type = ["mp4", "mov"])
 if uploaded_file:
-    st.video("temp_video.mp4")
+    # st.video("temp_video.mp4")
     responseJson = requests.post(URL, files = {"file": uploaded_file.read()})
 
     response = responseJson.json()
